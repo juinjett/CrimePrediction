62193ffd3d30fbb2029aa19177b57c01111fe5e0
Yi Li <featherlies@gmail.com>
2017-04-22 16:22:23 -0400

    update read_prj_data.py

--- read_prj_data.py
+++ read_prj_data.py
@@ -1,10 +1,29 @@
-import pycrs
+import matplotlib.pyplot as plt
 from pyproj import Proj, transform
 from read_dbf_data import get_rows
 
 inProj = Proj(init='epsg:2913', preserve_units=True)
 outProj = Proj(init='epsg:4326')
 rows = get_rows()
+lon_orig = []
+lon_conv= []
+lat_orig = []
+lat_conv = []
 for r in rows:
+    lon_orig.append(r[5])
+    lat_orig.append(r[6])
     lon, lat = transform(inProj, outProj, r[5], r[6])
+    lat_conv.append(lat)
+    lon_conv.append(lon)
     print lat, lon
+
+'''
+plt.subplot(2, 2, 1)
+plt.plot(lon_orig, lon_conv, 'o')
+plt.title('longitude')
+
+plt.subplot(2, 2, 2)
+plt.plot(lat_orig, lat_conv, 'ro')
+plt.title('lattitude')
+plt.show()
+'''
