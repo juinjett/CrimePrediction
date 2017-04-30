10155b66bd7d62b604062a464ec3ee6d808044f7
juinjett <juinjett@gmail.com>
2017-04-29 21:36:41 -0400

    update

--- junjie_test.m
+++ junjie_test.m
@@ -11,6 +11,13 @@ Y1 = [1 2 3 4]';
 Y2 = [4 3 2 1]';
 Y3 = [9 7 8 6]';
 
-Y = [Y1 Y2 Y3];
+Y = [Y1 Y2 Y3]
+tbl = table(Y1, Y2, Y3)
+Y2_sorted = sortrows(Y,-1);
 
-Y2_sorted = sortrows(Y,-1)
+%figure();
+% h = heatmap(tbl,'aaa','bbb');
+
+HeatMap(Y);
+% hold on;
+% HeatMap(Y2_sorted);
